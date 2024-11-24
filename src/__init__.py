f7e0f2d54b28a8280c729d637d1a6d147c3b0f07
janeckijakub <139973187+janeckijakub@users.noreply.github.com>
2024-11-24 11:22:58 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,6 @@
- 
+from . import app
+from . import llm_client
+from . import context_manager
+from . import query_processor
+
+__all__ = ['app', 'llm_client', 'context_manager', 'query_processor'] 
